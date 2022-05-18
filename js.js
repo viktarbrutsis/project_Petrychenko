8c0ce25b9cb221a6b171a190cadeecc3b8300dc3
Viktar <viktarbrutsis@gmail.com>
2022-05-18 16:57:18 +0300

    Add all files to _start_

---
+++ js.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//alert('Hello World!!!');
+
+//const result = confirm('Are you here?');
+//alert(result);
+
+//const answer = +prompt("Вам есть 18?", "");
+//console.log(typeof(answer));
+//const answers = [];
+
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваша фамилия', '');
+//answers[2] = prompt('Сколько вам лет', '');
+
+//console.log(typeof(answers));
+
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category}`);
+
+//const user = "Viktar";
+
+//alert(`Privet, ${user}`);
+
+//
+let incr = 10,
+    decr = 15;
+
+//++incr;
+//--decr;
+console.log(incr++);
+console.log(decr--);
